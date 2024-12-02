7bfc943b08175b9986f764383639b3eeafd71649
Nils Svenungsson <Nilssvenungsson@gmail.com>
2024-12-02 09:34:22 +0100

    New stuff

--- js/Game.js
+++ js/Game.js
@@ -10,8 +10,8 @@ export default class Game {
         this.player1InputNumberAnswer = //Variable of the answer for number inputs
         this.player2InputNumberAnswer = //Variable of the answer for number inputs
         this.correctInputAnswer = //Variable of the answer for number inputs
-        player1InputNumberAnswer = Number(inputNumberAnswer);
-        player2InputNumberAnswer = Number(inputNumberAnswer);
+        player1InputNumberAnswer = Number(player1InputNumberAnswer);
+        player2InputNumberAnswer = Number(player2InputNumberAnswer);
         correctInputAnswer = Number(correctInputAnswer);
 
         this.player1ScoreElement = document.getElementById("player1Score");
@@ -25,15 +25,22 @@ export default class Game {
     }
 
     //NOT DONE YET BUDDY!
-    checkClosestOnSuddenDeath() {
-        var array = [player1InputNumberAnswer, player2InputNumberAnswer];
-        for (correctInputAnswer in array) {
-            //WRITE CODE!
+    checkClosestOnSuddenDeath(player1InputNumberAnswer, player2InputNumberAnswer, correctInputAnswer) {
+        Math.abs(player1InputNumberAnswer - correctInputAnswer) = absCheckClosestNumberPlayer1;
+        Math.abs(player2InputNumberAnswer - correctInputAnswer) = absCheckClosestNumberPlayer2;
+        if (absCheckClosestNumberPlayer1 < absCheckClosestNumberPlayer2) {
+            player1Points += 1;
+        }
+        if (absCheckClosestNumberPlayer2 < absCheckClosestNumberPlayer1) {
+            player2Points += 1;
+
         }
     }
+
 
     updateScore() {
         player1ScoreElement.innerHTML = player1Points;
         player2ScoreElement.innerHTML = player2Points;
     }
 }
+
