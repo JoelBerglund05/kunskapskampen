ff0268782ef06c7a4b6eb3442b08a607c5846780
Ebbe Johansson <ebbe.johansson1@gmail.com>
2024-12-04 21:52:41 +0100

    Kinda done
    I just have one thing i wanna fit in but i might need to ask Joel about it. It is simple but i am stupid.

--- mail.js
+++ mail.js
@@ -8,11 +8,12 @@ const validate = () => {
 
     if(validateEmail(email)){
         result.textContent = `${email} is valid`;
-        result.style.color = 'green';
+        result.style.color = 'oklch( 70% 0.1776 141.88)';
     }
     else {
         result.textContent = `${email} is invalid`;
-        result.style.color = 'red';
+        result.style.color = 'oklch( 60% 0.1825 21.18)';
+        email.style.color = 'oklch( 70% 0.1776 141.88)';
     }
     return false;
 }
