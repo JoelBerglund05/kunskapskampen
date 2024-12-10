10c31c07bd7675c0688a083a5741c70384ce821a
Johanssono <ottojohanssonjobb@gmail.com>
2024-12-10 15:41:18 +0100

    prettier

--- js/main.js
+++ js/main.js
@@ -1,30 +1,40 @@
-class Main{
-    RegisterServiceWorker() {
-        if('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('./js/ServiceWorker.js').then((reg) => {
-               console.log('Registration succeeded. Scope is ' + reg.scope);
-            });
-        }
+class Main {
+  RegisterServiceWorker() {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("./js/ServiceWorker.js").then((reg) => {
+        console.log("Registration succeeded. Scope is " + reg.scope);
+      });
     }
+  }
 }
 
 const main = new Main();
 main.RegisterServiceWorker();
 
-
 function ColorCheck() {
-    if ("11" == (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1] || "10" == (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/)||[,null])[1]) {
-      if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-        document.documentElement.setAttribute("data-theme", "dark");
-      } else {
-        document.documentElement.setAttribute("data-theme", "light");
-      }
-    } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-      document.documentElement.setAttribute("data-theme", "light");
-    } else {
+  if (
+    "11" ==
+      (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/) || [
+        ,
+        null,
+      ])[1] ||
+    "10" ==
+      (document.cookie.match(/^(?:.*;)?\s*Color\s*=\s*([^;]+)(?:.*)?$/) || [
+        ,
+        null,
+      ])[1]
+  ) {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
       document.documentElement.setAttribute("data-theme", "dark");
+    } else {
+      document.documentElement.setAttribute("data-theme", "light");
     }
+  } else if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+    document.documentElement.setAttribute("data-theme", "light");
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
   }
+}
 var dark = false;
 
 function darkmode() {
@@ -49,6 +59,3 @@ function darkmode() {
   }
   ColorCheck();
 }
-
-
-
