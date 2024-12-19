3b38823608c84ba6c2ce13b04dfbb51491cdf91e
Ebbe Johanssson <mr.ebbe.johansson@gmail.com>
2024-12-19 08:43:31 +0100

    klar
    typ klar

--- js/GameHandeler.js
+++ js/GameHandeler.js
@@ -121,18 +121,14 @@ export default class GameHandeler {
         answersBtns.forEach((button) => button.classList.remove("selected"));  
         btn.classList.add("selected");  
         selectedAnswer = btn.innerText;
-        console.log(`Answer selected: ${selectedAnswer}`);
       });
     });
   
     submitBtn.addEventListener("click", () => {
       if (selectedAnswer) {
-        console.log(`Submitting answer: ${selectedAnswer}`);        
         this.ButtonAnswer(selectedAnswer);
           selectedAnswer = null;
         answersBtns.forEach((button) => button.classList.remove("selected"));
-      } else {
-        console.log("No answer selected. Please select an answer before submitting.");
       }
     });
   }
