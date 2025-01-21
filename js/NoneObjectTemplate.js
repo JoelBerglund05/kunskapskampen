9b5513532750c8990cf41049e72bcd47c5136be6
Nils Svenungsson <Nilssvenungsson@gmail.com>
2025-01-21 15:55:24 +0100

    more on the template script and html document

--- js/NoneObjectTemplate.js
+++ js/NoneObjectTemplate.js
@@ -1,11 +1,15 @@
 const template = document.getElementById("score-template");
 const clone = document.importNode(template.content, true);
 
+
 const yourScore = 0;
+const yourCategory1Score = 0;
 const opponentScore = 0;
 
-clone.querySelector('p').textContent = '${yourScore}';
+clone.querySelector('#yourScore').textContent = '${yourScore}';
+clone.querySelector('#opponentScore').textContent = '${opponentScore}';
 
 document.getElementById('content').appendChild(clone);
+
 
 console.log('TEMPLATES');
