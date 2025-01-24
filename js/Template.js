ee7fef2861bffcd93876e25cb491817ec7edb31e
Nils Svenungsson <Nilssvenungsson@gmail.com>
2025-01-24 11:52:25 +0100

    now objectoriented

--- js/Template.js
+++ js/Template.js
@@ -1,18 +1,27 @@
 export default class Template {
     constructor() {
-        this.tpl = getElementById('score-template');
-        this.clonedTr = tpl.content.cloneNode(true);
+        this.template = document.getElementById("score-template");
+        this.clone = document.importNode(template.content, true);
+
+        this. yourScore = 0;
+        this. yourCategory1Score = 0;
+        this. yourCategory2Score = 0;
+        this. yourCategory3Score = 0;
+        this. opponentScore = 0;
+        this. opponentCategory1Score = 0;
+        this. opponentCategory2Score = 0;
+        this. opponentCategory3Score = 0;
     }
-    scoreTemplate(){
-        clonedTr.querySelectorAll('td').forEach((tdElem, i) => {
-            if (i === 0) {
-                tdElem.innerText = 'You';
-            }
-            else {
-                tdElem.innerText = 'Opponant';
-            }
-        });
-    
-        document.querySelector('table tbody').appendChild(clonedTr);
+    templateCreator(){
+        clone.querySelector('#yourScore').textContent = yourScore;
+        clone.querySelector('#opponentScore').textContent = opponentScore;
+        clone.querySelector('#yourCategory1Score').textContent = yourCategory1Score;
+        clone.querySelector('#opponentCategory1Score').textContent = opponentCategory1Score;
+        clone.querySelector('#yourCategory2Score').textContent = yourCategory2Score;
+        clone.querySelector('#opponentCategory2Score').textContent = opponentCategory2Score;
+        clone.querySelector('#yourCategory3Score').textContent = yourCategory3Score;
+        clone.querySelector('#opponentCategory3Score').textContent = opponentCategory3Score;
+
+        document.getElementById('content').appendChild(clone);
     }
 }
