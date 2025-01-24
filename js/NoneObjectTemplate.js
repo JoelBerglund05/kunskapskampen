0894e92d8f5af489b5c36476fa42158caa85c44d
Nils Svenungsson <Nilssvenungsson@gmail.com>
2025-01-24 11:37:05 +0100

    template for score in none objectoriented is done

--- js/NoneObjectTemplate.js
+++ js/NoneObjectTemplate.js
@@ -1,13 +1,24 @@
 const template = document.getElementById("score-template");
 const clone = document.importNode(template.content, true);
 
+let yourScore = 0;
+let yourCategory1Score = 0;
+let yourCategory2Score = 0;
+let yourCategory3Score = 0;
+let opponentScore = 0;
+let opponentCategory1Score = 0;
+let opponentCategory2Score = 0;
+let opponentCategory3Score = 0;
 
-const yourScore = 0;
-const yourCategory1Score = 0;
-const opponentScore = 0;
+clone.querySelector('#yourScore').textContent = yourScore;
+clone.querySelector('#opponentScore').textContent = opponentScore;
+clone.querySelector('#yourCategory1Score').textContent = yourCategory1Score;
+clone.querySelector('#opponentCategory1Score').textContent = opponentCategory1Score;
+clone.querySelector('#yourCategory2Score').textContent = yourCategory2Score;
+clone.querySelector('#opponentCategory2Score').textContent = opponentCategory2Score;
+clone.querySelector('#yourCategory3Score').textContent = yourCategory3Score;
+clone.querySelector('#opponentCategory3Score').textContent = opponentCategory3Score;
 
-clone.querySelector('#yourScore').textContent = '${yourScore}';
-clone.querySelector('#opponentScore').textContent = '${opponentScore}';
 
 document.getElementById('content').appendChild(clone);
 
