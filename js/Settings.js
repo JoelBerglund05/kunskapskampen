cc5813fcf6860e91a3a6bdb4f23ed82b4a3a069e
Johanssono <ottojohanssonjobb@gmail.com>
2025-01-31 11:45:37 +0100

    I

--- js/Settings.js
+++ js/Settings.js
@@ -1,1 +1,22 @@
-class Settings {}
+function darkmode() {
+    if (dark) {
+        dark = false;
+        document.getElementById("moon").classList.remove("sun");
+        document.getElementById("tdnn").classList.remove("day");
+        if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+            document.cookie = "Color=11;";
+        } else {
+            document.cookie = "Color=01;";
+        }
+    } else {
+        dark = true;
+        document.getElementById("moon").classList.add("sun");
+        document.getElementById("tdnn").classList.add("day");
+        if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+            document.cookie = "Color=10;";
+        } else {
+            document.cookie = "Color=00;";
+        }
+    }
+    ColorCheck();
+}
