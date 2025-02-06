c9a8cd188af1637429ddd42f06e9f2b597fb5bea
Johanssono <ottojohanssonjobb@gmail.com>
2025-02-06 19:46:56 +0100

    layout and sound
    Fixed layout on settings screen and added audio player with javascript to keep the music going between pages

--- js/Settings.js
+++ js/Settings.js
@@ -1,3 +1,66 @@
+function setCookie(c_name, value, exdays) {
+    var exdate = new Date();
+    exdate.setDate(exdate.getDate() + exdays);
+    var c_value = escape(value) +
+        ((exdays == null) ? "" : "; expires=" + exdate.toUTCString());
+    document.cookie = c_name + "=" + c_value;
+}
+
+function getCookie(c_name) {
+    var i, x, y, ARRcookies = document.cookie.split(";");
+    for (i = 0; i < ARRcookies.length; i++) {
+        x = ARRcookies[i].substr(0, ARRcookies[i].indexOf("="));
+        y = ARRcookies[i].substr(ARRcookies[i].indexOf("=") + 1);
+        x = x.replace(/^\s+|\s+$/g, "");
+        if (x == c_name) {
+            return unescape(y);
+        }
+    }
+}
+
+function isPlaying(audioEl) {
+    return !audioEl.paused;
+}
+
+var myAudio = document.getElementById('audioplayer');
+
+var song = document.getElementsByTagName('audio')[0];
+var played = false;
+var tillPlayed = getCookie('timePlayed');
+function update() {
+    if (!played) {
+        if (tillPlayed) {
+            song.currentTime = tillPlayed;
+            played = true;
+            if (!myAudio.paused) {
+                song.play();
+                played = true;
+            }
+            else {
+                played = true
+            }
+
+        }
+        else {
+            if (!myAudio.paused) {
+                song.play();
+                played = true;
+            }
+
+            else {
+                played = true;
+            }
+            played = true;
+        }
+    }
+
+    else {
+        setCookie('timePlayed', song.currentTime);
+    }
+}
+setInterval(update, 1000);
+
+
 function ColorCheck() {
     if (
         "11" ==
